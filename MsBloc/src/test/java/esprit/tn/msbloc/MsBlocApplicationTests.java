f647a71371d1461826f96ff0af14d07d7493a5a5
mekni99 <mohameddhia.mekni@esprit.tn>
2023-12-13 21:35:40 +0000

    msbloc

---
+++ MsBloc/src/test/java/esprit/tn/msbloc/MsBlocApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.msbloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBlocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
